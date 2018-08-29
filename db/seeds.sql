02882c492e5f129ab74e28d3e09b69e21d156d5c
Michael LeGrange <mlegrange15@att.net>
2018-08-29 11:29:23 -0500

    have the db and config folder set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buzz Lightyear', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Wreck-It-Ralph', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lightning McQueen', true);
